5c0247f530de60916b12f823a52dd21d899b35d4
nandhakumar <nandhakumar7thjuly@gmail.com>
2019-10-27 19:02:31 +0530

    Restructing folders and code cleanup

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import CardList from './CardList/CardList';
-import SearchBox from './SearchBox/SearchBox';
-import Scroll from './Scroll/Scroll';
+import CardList from '../components/CardList/CardList'
+import SearchBox from '../components/SearchBox/SearchBox';
+import Scroll from '../components/Scroll/Scroll';
 
 class App extends Component{
   constructor(props){
@@ -29,15 +29,14 @@ class App extends Component{
 
 
   render() {
-    const filteredRobots = this.state.robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
 
-    if(this.state.robots.length === 0){
-      return <h1>Loading</h1>
-    }
-    else {
-      return (
+    return (!robots.length) ?
+      <h1>Loading</h1> :
+      (
         <div className="tc">
           <h1 className="f1">RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
@@ -46,7 +45,6 @@ class App extends Component{
           </Scroll>
         </div>
       );
-    }
   }
 }
 
