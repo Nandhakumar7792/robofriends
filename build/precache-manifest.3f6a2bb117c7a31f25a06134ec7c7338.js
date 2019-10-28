133a82b905537cba50b9317bd3a9da48de87d0ac
nandhakumar <nandhakumar7thjuly@gmail.com>
2019-10-28 11:43:30 +0530

    deploying to github pages

---
+++ build/precache-manifest.3f6a2bb117c7a31f25a06134ec7c7338.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645daef5ed69ff12a53a02fe3b2d8077",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "dd24a9a35007893af7ff",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "77b887cc2d67247690c3",
+    "url": "/robofriends/static/css/main.7fc636ec.chunk.css"
+  },
+  {
+    "revision": "dd24a9a35007893af7ff",
+    "url": "/robofriends/static/js/2.f3e53e03.chunk.js"
+  },
+  {
+    "revision": "77b887cc2d67247690c3",
+    "url": "/robofriends/static/js/main.c32eec78.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
